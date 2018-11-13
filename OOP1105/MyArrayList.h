b8c5d4cf9fd3781a9da1cc05bff7ce58d39f54ed
henry9489 <henry9489@gmail.com>
2018-11-13 21:51:02 +0900

    OOP_test
    test

---
+++ OOP1105/MyArrayList.h
@@ -0,0 +1,55 @@
+#ifndef MYARRAYLIST_H
+#define MYARRAYLIST_H
+
+using namespace std;
+
+template <typename T>
+class MyArrayList {
+public:
+	MyArrayList();
+	MyArrayList(int x);
+	bool addElement(T const &x);
+	void printIt();
+
+private:
+	const static int DEFAULT_CAPACITY = 10;
+	T *mass;
+	int capacity = 0;
+	int size = 0;
+};
+
+#endif
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+template<class T>
+MyArrayList<T>::MyArrayList() {
+	capacity = DEFAULT_CAPACITY;
+	mass = new T[capacity];
+	size = 0;
+	
+}
+template<class T>
+MyArrayList<T>::MyArrayList(int x) {
+	capacity = x;
+	mass = new T[capacity];
+	size = 0;
+	
+}
+template<class T>
+bool MyArrayList<T>::addElement(T const &x) {
+	mass[size] = x;
+	size++;
+	return true;
+}
+template<class T>
+void MyArrayList<T>::printIt() {
+	cout << "{";
+	for (int i = 0; i < size; i++) {
+		cout << mass[i] << " ";
+	}
+	cout << "}" << endl;
+}
